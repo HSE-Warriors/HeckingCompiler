159a2c3621f925b17bc6ada241db8ba038b6848d
Александр Кошкин <85771670+koshkinHSE@users.noreply.github.com>
2021-06-22 00:47:02 +0300

    Uploaded main.cpp
    Размеченный main() для дальнейшей сборки

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Common I/O
+char Files[50][40] = { 0 }; //Для Лёши, чтобы обмениваться с остальными
+int AmountOfFiles = 1; ///Для Лёши, чтобы передать Дену
+char Functions[50][50] = { 0 }; int AmountOfFunctions = 0; //Для Саши, чтобы передать Дену
+
+int main()
+{
+	//Функции Лёши
+
+	//Функции Саши
+
+	//Функции Дена
+
+	//функции Гоши
+
+	//вывод статистики
+
+	return 0;
+}
