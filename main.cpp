e07ea038719a8bdbf2ac021e576662c2843dbcce
Denis Kovsharev <2323set@gmail.com>
2021-06-27 22:09:24 +0300

    Change main for my functions, update README
    Problems #5

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 char Files[50][40] = { 0 }; //Для Лёши, чтобы обмениваться с остальными
 int AmountOfFiles = 1; ///Для Лёши, чтобы передать Дену
 char Functions[50][50] = { 0 }; int AmountOfFunctions = 0; //Для Саши, чтобы передать Дену
+int AmountOfFunctions = 0;//Для Дена
+char InfiniteLoopSrtings[50][100] = { 0 };
+int AmountOfInfiniteMessages = 0;
+
+NODE* MassOfNodes[50] = { 0 };
+NODE* MassOfRoots[50] = { 0 };
+int CountRoots = 0;
 
 int main()
 {
@@ -15,7 +22,10 @@ int main()
 	//Функции Саши
 
 	//Функции Дена
-
+	
+	FunctionsGraph(Files, AmountOfFiles, Functions, AmountOfFunctions, MassOfNodes, MassOfRoots, &CountRoots);
+	InfiniteLoop(Files, AmountOfFiles, InfiniteLoopSrtings, &AmountOfInfiniteMessages);
+	
 	//функции Гоши
 
 	//вывод статистики
